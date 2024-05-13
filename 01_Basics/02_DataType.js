47099cfb018246757ac170d0f23338cb812a5cae
rakhi prasad <142569721+Prasadrakhi2@users.noreply.github.com>
2024-05-13 12:54:23 +0000

    data types

---
+++ 01_Basics/02_DataType.js
@@ -0,0 +1,14 @@
+"use strict";  //use all the new version js code
+
+alert(4+5)  //only use in the browsers
+
+let name = "rakhi"  //only strings are writen in ""
+console.log(typeof name)
+
+let age = 18
+console.log(typeof age)
+
+console.table([name,age])//
+
+//typeOf  => use to find the type of any variables
+
